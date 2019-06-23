8b71661f2b00980b952e90d2764763e2716cf224
Marcos Cruz <github@programandala.net>
2019-06-23 18:28:03 +0200

    Make the layouts more legible in the source

--- src/keymap.c
+++ src/keymap.c
@@ -1,6 +1,6 @@
 // programandala.net ErgoDox EZ
-// Version 0.2.0+201906231724
- 
+// Version 0.2.0+201906231825
+
 // By Marcos Cruz (programandala.net), 2019
 
 // 2019-06-23
@@ -45,318 +45,96 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ergodox(
 
-	// Left hand, row 1:
-	ES_IEXL,
-	KC_1,
-	KC_2,
-	KC_3,
-	KC_4,
-	KC_5,
-	KC_TRANSPARENT,
+  // .........................
+  // Left hand
 
-	// Left hand, row 2:
-	KC_CAPSLOCK,
-	KC_Q,
-	KC_W,
-	KC_E,
-	KC_R,
-	KC_T,
-	KC_ESCAPE,
+  ES_IEXL,         KC_1,      KC_2,      KC_3,      KC_4,       KC_5,   KC_TRANSPARENT,
+  KC_CAPSLOCK,     KC_Q,      KC_W,      KC_E,      KC_R,       KC_T,   KC_ESCAPE,
+  KC_TAB,          KC_A,      KC_S,      KC_D,      KC_F,       KC_G,
+  OSM(MOD_LSFT),   KC_Z,      KC_X,      KC_C,      KC_V,       KC_B,   KC_TRANSPARENT,
+  TG(1),           ES_OVRR,   ES_LESS,   KC_LEFT,   KC_RIGHT,
 
-	// Left hand, row 3:
-	KC_TAB,
-	KC_A,
-	KC_S,
-	KC_D,
-	KC_F,
-	KC_G,
+             OSM(MOD_LCTL), OSM(MOD_LALT),
+                            KC_HOME,
+  KC_BSPACE, KC_DELETE,     KC_END,
 
-	// Left hand, row 4:
-	OSM(MOD_LSFT),
-	KC_Z,
-	KC_X,
-	KC_C,
-	KC_V,
-	KC_B,
-	KC_TRANSPARENT,
+  // .........................
+  // Right hand
 
-	// Left hand, row 5:
-	TG(1),
-	ES_OVRR,
-	ES_LESS,
-	KC_LEFT,
-	KC_RIGHT,
+  KC_TRANSPARENT,   KC_6,      KC_7,     KC_8,       KC_9,      KC_0,      ES_APOS,
+  KC_ESCAPE,        KC_Y,      KC_U,     KC_I,       KC_O,      KC_P,      ES_CCED,
+                    KC_H,      KC_J,     KC_K,       KC_L,      ES_NTIL,   ES_ACUT,
+  KC_TRANSPARENT,   KC_N,      KC_M,     KC_COMMA,   KC_DOT,    ES_MINS,   OSM(MOD_RSFT),
+                               KC_UP,    KC_DOWN,    ES_GRV,    ES_PLUS,   TG(2),
 
-	// Left thumb:
-	OSM(MOD_LCTL),
-	OSM(MOD_LALT),
-	KC_HOME,
-	KC_BSPACE,
-	KC_DELETE,
-	KC_END,
-
-	// Right hand, row 1:
-	KC_TRANSPARENT,
-	KC_6,
-	KC_7,
-	KC_8,
-	KC_9,
-	KC_0,
-	ES_APOS,
-
-	// Right hand, row 2:
-	KC_ESCAPE,
-	KC_Y,
-	KC_U,
-	KC_I,
-	KC_O,
-	KC_P,
-	ES_CCED,
-
-	// Right hand, row 3:
-	KC_H,
-	KC_J,
-	KC_K,
-	KC_L,
-	ES_NTIL,
-	ES_ACUT,
-
-	// Right hand, row 4:
-	KC_TRANSPARENT,
-	KC_N,
-	KC_M,
-	KC_COMMA,
-	KC_DOT,
-	ES_MINS,
-	OSM(MOD_RSFT),
-
-	// Right hand, row 5:
-	KC_UP,
-	KC_DOWN,
-	ES_GRV,
-	ES_PLUS,
-	TG(2),
-
-	// Right thumb:
-	OSM(MOD_RALT),
-	OSM(MOD_RCTL),
-	KC_PGUP,
-	KC_PGDOWN,
-	KC_ENTER,
-	KC_SPACE),
+  OSM(MOD_RALT), OSM(MOD_RCTL),
+  KC_PGUP,
+  KC_PGDOWN,     KC_ENTER,      KC_SPACE),
 
 // ---------------------------------------------
 // Layout 1: Functions and movement
 
   [1] = LAYOUT_ergodox(
 
-	// Left hand, row 1:
-	KC_F12,
-	KC_F1,
-	KC_F2,
-	KC_F3,
-	KC_F4,
-	KC_F5,
-	KC_TRANSPARENT,
+  // .........................
+  // Left hand
 
-	// Left hand, row 2:
-	KC_TRANSPARENT,
-	KC_MS_WH_LEFT,
-	KC_MS_WH_UP,
-	KC_MS_WH_DOWN,
-	KC_MS_WH_RIGHT,
-	KC_PC_CUT,
-	KC_ESCAPE,
+  KC_F12,         KC_F1,          KC_F2,          KC_F3,         KC_F4,          KC_F5,         KC_TRANSPARENT,
+  KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_MS_WH_UP,    KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_PC_CUT,     KC_ESCAPE,
+  KC_TAB,         KC_MS_LEFT,     KC_MS_UP,       KC_MS_DOWN,    KC_MS_RIGHT,    KC_PC_COPY,
+  OSM(MOD_LSFT),  KC_LEFT,        KC_UP,          KC_DOWN,       KC_RIGHT,       KC_PC_PASTE,   KC_TRANSPARENT,
+  TG(1),          KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,       KC_RIGHT,
 
-	// Left hand, row 3:
-	KC_TAB,
-	KC_MS_LEFT,
-	KC_MS_UP,
-	KC_MS_DOWN,
-	KC_MS_RIGHT,
-	KC_PC_COPY,
+              KC_MS_BTN3, KC_TRANSPARENT,
+                          KC_HOME,
+  KC_MS_BTN2, KC_MS_BTN1, KC_END,
 
-	// Left hand, row 4:
-	OSM(MOD_LSFT),
-	KC_LEFT,
-	KC_UP,
-	KC_DOWN,
-	KC_RIGHT,
-	KC_PC_PASTE,
-	KC_TRANSPARENT,
+  // .........................
+  // Right hand
 
-	// Left hand, row 5:
-	TG(1),
-	KC_TRANSPARENT,
-	KC_TRANSPARENT,
-	KC_LEFT,
-	KC_RIGHT,
+  KC_TRANSPARENT, KC_F6,       KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
+  KC_ESCAPE,      KC_PC_CUT,   KC_MS_WH_LEFT,  KC_MS_WH_DOWN,  KC_MS_WH_UP,    KC_MS_WH_RIGHT, KC_TRANSPARENT,
+                  KC_PC_COPY,  KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    KC_TAB,
+  KC_TRANSPARENT, KC_PC_PASTE, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       OSM(MOD_RSFT),
+                               KC_UP,          KC_DOWN,        KC_TRANSPARENT, KC_TRANSPARENT, TG(2),
 
-	// Left thumb:
-	KC_MS_BTN3,
-	KC_TRANSPARENT,
-	KC_HOME,
-	KC_MS_BTN2,
-	KC_MS_BTN1,
-	KC_END,
+  KC_TRANSPARENT, KC_MS_BTN3,
+  KC_PGUP,
+  KC_PGDOWN,      KC_MS_BTN1, KC_MS_BTN2),
 
-	// Right hand, row 1:
-	KC_TRANSPARENT,
-	KC_F6,
-	KC_F7,
-	KC_F8,
-	KC_F9,
-	KC_F10,
-	KC_F11,
-
-	// Right hand, row 2:
-	KC_ESCAPE,
-	KC_PC_CUT,
-	KC_MS_WH_LEFT,
-	KC_MS_WH_DOWN,
-	KC_MS_WH_UP,
-	KC_MS_WH_RIGHT,
-	KC_TRANSPARENT,
-
-	// Right hand, row 3:
-	KC_PC_COPY,
-	KC_MS_LEFT,
-	KC_MS_DOWN,
-	KC_MS_UP,
-	KC_MS_RIGHT,
-	KC_TAB,
-
-	// Right hand, row 4:
-	KC_TRANSPARENT,
-	KC_PC_PASTE,
-	KC_LEFT,
-	KC_DOWN,
-	KC_UP,
-	KC_RIGHT,
-	OSM(MOD_RSFT),
-
-	// Right hand, row 5:
-	KC_UP,
-	KC_DOWN,
-	KC_TRANSPARENT,
-	KC_TRANSPARENT,
-	TG(2),
-
-	// Right thumb:
-	KC_TRANSPARENT,
-	KC_MS_BTN3,
-	KC_PGUP,
-	KC_PGDOWN,
-	KC_MS_BTN1,
-	KC_MS_BTN2),
-	
 // ---------------------------------------------
 // Layout 2: Spanish Dvorak
 
 // This layout is needed only when the Spanish Dvorak layout is not already
 // selected in the OS.
 
-	[2] = LAYOUT_ergodox(
+  [2] = LAYOUT_ergodox(
 
-	// Left hand, row 1:
-	ES_IEXL,
-	KC_1,
-	KC_2,
-	KC_3,
-	KC_4,
-	KC_5,
-	KC_TRANSPARENT,
+  // .........................
+  // Left hand
 
-	// Left hand, row 2:
-	KC_CAPSLOCK,
-	KC_KP_DOT,
-	KC_COMMA,
-	ES_NTIL,
-	KC_P,
-	KC_Y,
-	KC_ESCAPE,
+  ES_IEXL,        KC_1,       KC_2,      KC_3,     KC_4,      KC_5,  KC_TRANSPARENT,
+  KC_CAPSLOCK,    KC_KP_DOT,  KC_COMMA,  ES_NTIL,  KC_P,      KC_Y,  KC_ESCAPE,
+  KC_TAB,         KC_A,       KC_O,      KC_E,     KC_U,      KC_I,
+  OSM(MOD_LSFT),  ES_MINS,    KC_Q,      KC_J,     KC_K,      KC_X,  KC_TRANSPARENT,
+  TG(1),          ES_OVRR,    ES_LESS,   KC_LEFT,  KC_RIGHT,
 
-	// Left hand, row 3:
-	KC_TAB,
-	KC_A,
-	KC_O,
-	KC_E,
-	KC_U,
-	KC_I,
+             OSM(MOD_LCTL), OSM(MOD_LALT),
+                            KC_HOME,
+  KC_BSPACE, KC_DELETE,     KC_END,
 
-	// Left hand, row 4:
-	OSM(MOD_LSFT),
-	ES_MINS,
-	KC_Q,
-	KC_J,
-	KC_K,
-	KC_X,
-	KC_TRANSPARENT,
+  // .........................
+  // Right hand
 
-	// Left hand, row 5:
-	TG(1),
-	ES_OVRR,
-	ES_LESS,
-	KC_LEFT,
-	KC_RIGHT,
+  KC_TRANSPARENT,  KC_6,  KC_7,    KC_8,     KC_9,    KC_0,     ES_APOS,
+  KC_ESCAPE,       KC_F,  KC_G,    KC_C,     KC_H,    KC_L,     ES_CCED,
+                   KC_D,  KC_R,    KC_T,     KC_N,    KC_S,     ES_ACUT,
+  KC_TRANSPARENT,  KC_B,  KC_M,    KC_W,     KC_V,    KC_Z,     OSM(MOD_RSFT),
+                          KC_UP,   KC_DOWN,  ES_GRV,  ES_PLUS,  TG(2),
 
-	// Left thumb:
-	OSM(MOD_LCTL),
-	OSM(MOD_LALT),
-	KC_HOME,
-	KC_BSPACE,
-	KC_DELETE,
-	KC_END,
-
-	// Right hand, row 1:
-	KC_TRANSPARENT,
-	KC_6,
-	KC_7,
-	KC_8,
-	KC_9,
-	KC_0,
-	ES_APOS,
-
-	// Right hand, row 2:
-	KC_ESCAPE,
-	KC_F,
-	KC_G,
-	KC_C,
-	KC_H,
-	KC_L,
-	ES_CCED,
-
-	// Right hand, row 3:
-	KC_D,
-	KC_R,
-	KC_T,
-	KC_N,
-	KC_S,
-	ES_ACUT,
-
-	// Right hand, row 4:
-	KC_TRANSPARENT,
-	KC_B,
-	KC_M,
-	KC_W,
-	KC_V,
-	KC_Z,
-	OSM(MOD_RSFT),
-
-	// Right hand, row 5:
-	KC_UP,
-	KC_DOWN,
-	ES_GRV,
-	ES_PLUS,
-	TG(2),
-
-	// Right thumb:
-	RALT_T(KC_NO),
-	RCTL_T(KC_NO),
-	KC_PGUP,
-	KC_PGDOWN,
-	KC_ENTER,
-	KC_SPACE),
+  RALT_T(KC_NO), RCTL_T(KC_NO),
+  KC_PGUP,
+  KC_PGDOWN,     KC_ENTER,      KC_SPACE),
 };
 
 // =============================================================
