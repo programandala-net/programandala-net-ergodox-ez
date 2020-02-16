d9a47cccc7b61355af7c1f934ec21a2a5cf04ced
Marcos Cruz <github@programandala.net>
2020-02-16 22:05:36 +0100

    Rewrite the tool layer with one-shot function keys
    Since my system is keyboard-driven, and even the graphic pointer can be
    handled by the keyboard (with keynav), the misc layer was useless,
    except for the function keys, which now are one-shot, easier to use, and
    accessible from both main layes.

--- src/keymap.c
+++ src/keymap.c
@@ -1,7 +1,7 @@
 // programandala.net ErgoDox EZ
-// Version 0.3.1+201909141908
+// Version 0.4.0+202002162146
 
-// By Marcos Cruz (programandala.net), 2019
+// By Marcos Cruz (programandala.net), 2019,2020
 
 // http://programandala.net/es.programa.programandala_net_ergodox_ez.html
 
@@ -25,6 +25,10 @@
 #define NO_TH ALGR(KC_T)
 #define NO_ETH ALGR(KC_D)
 
+#define _QL 0 // Spanish Qwerty layer
+#define _DL 1 // Spanish Dvorak layer
+#define _FL 2 // Function keys layer
+
 enum custom_keycodes {
   RGB_SLD = SAFE_RANGE, // can always be here
   TOGGLE_LAYER_COLOR,
@@ -43,16 +47,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // already selected in the OS, which is the case with my computers. That's why
 // this is the first layer.
 
-  [0] = LAYOUT_ergodox(
+  [_QL] = LAYOUT_ergodox(
 
   // .........................
   // Left hand
 
-  ES_IEXL,         KC_1,      KC_2,      KC_3,      KC_4,       KC_5,   KC_TRANSPARENT,
+  ES_IEXL,         KC_1,      KC_2,      KC_3,      KC_4,       KC_5,   KC_NO,
   KC_CAPSLOCK,     KC_Q,      KC_W,      KC_E,      KC_R,       KC_T,   KC_ESCAPE,
   KC_TAB,          KC_A,      KC_S,      KC_D,      KC_F,       KC_G,
-  OSM(MOD_LSFT),   KC_Z,      KC_X,      KC_C,      KC_V,       KC_B,   KC_TRANSPARENT,
-  TG(1),           ES_OVRR,   ES_LESS,   KC_LEFT,   KC_RIGHT,
+  OSM(MOD_LSFT),   KC_Z,      KC_X,      KC_C,      KC_V,       KC_B,   OSL(_FL),
+  TO(_DL),         ES_OVRR,   ES_LESS,   KC_LEFT,   KC_RIGHT,
 
              OSM(MOD_LCTL), OSM(MOD_LALT),
                             KC_HOME,
@@ -61,63 +65,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // .........................
   // Right hand
 
-  KC_TRANSPARENT,   KC_6,      KC_7,     KC_8,       KC_9,      KC_0,      ES_APOS,
+  KC_NO,            KC_6,      KC_7,     KC_8,       KC_9,      KC_0,      ES_APOS,
   KC_ESCAPE,        KC_Y,      KC_U,     KC_I,       KC_O,      KC_P,      ES_CCED,
                     KC_H,      KC_J,     KC_K,       KC_L,      ES_NTIL,   ES_ACUT,
-  KC_TRANSPARENT,   KC_N,      KC_M,     KC_COMMA,   KC_DOT,    ES_MINS,   OSM(MOD_RSFT),
-                               KC_UP,    KC_DOWN,    ES_GRV,    ES_PLUS,   TG(2),
+  OSL(_FL),         KC_N,      KC_M,     KC_COMMA,   KC_DOT,    ES_MINS,   OSM(MOD_RSFT),
+                               KC_UP,    KC_DOWN,    ES_GRV,    ES_PLUS,   TO(_DL),
 
   OSM(MOD_RALT), OSM(MOD_RCTL),
   KC_PGUP,
   KC_PGDOWN,     KC_ENTER,      KC_SPACE),
 
 // ---------------------------------------------
-// Layout 1: Functions and movement
-
-  [1] = LAYOUT_ergodox(
-
-  // .........................
-  // Left hand
-
-  KC_F12,         KC_F1,          KC_F2,          KC_F3,         KC_F4,          KC_F5,         KC_TRANSPARENT,
-  KC_TRANSPARENT, KC_MS_WH_LEFT,  KC_MS_WH_UP,    KC_MS_WH_DOWN, KC_MS_WH_RIGHT, KC_PC_CUT,     KC_ESCAPE,
-  KC_TAB,         KC_MS_LEFT,     KC_MS_UP,       KC_MS_DOWN,    KC_MS_RIGHT,    KC_PC_COPY,
-  OSM(MOD_LSFT),  KC_LEFT,        KC_UP,          KC_DOWN,       KC_RIGHT,       KC_PC_PASTE,   KC_TRANSPARENT,
-  TG(1),          KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,       KC_RIGHT,
-
-              KC_MS_BTN3, KC_TRANSPARENT,
-                          KC_HOME,
-  KC_MS_BTN2, KC_MS_BTN1, KC_END,
-
-  // .........................
-  // Right hand
-
-  KC_TRANSPARENT, KC_F6,       KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-  KC_ESCAPE,      KC_PC_CUT,   KC_MS_WH_LEFT,  KC_MS_WH_DOWN,  KC_MS_WH_UP,    KC_MS_WH_RIGHT, KC_F12,
-                  KC_PC_COPY,  KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    KC_TAB,
-  KC_TRANSPARENT, KC_PC_PASTE, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       OSM(MOD_RSFT),
-                               KC_UP,          KC_DOWN,        KC_TRANSPARENT, KC_TRANSPARENT, TG(2),
-
-  KC_TRANSPARENT, KC_MS_BTN3,
-  KC_PGUP,
-  KC_PGDOWN,      KC_MS_BTN1, KC_MS_BTN2),
-
-// ---------------------------------------------
-// Layout 2: Spanish Dvorak
+// Layout 1: Spanish Dvorak
 
 // This layout is needed only when the Spanish Dvorak layout is not already
 // selected in the OS.
 
-  [2] = LAYOUT_ergodox(
+  [_DL] = LAYOUT_ergodox(
 
   // .........................
   // Left hand
 
-  ES_IEXL,        KC_1,       KC_2,      KC_3,     KC_4,      KC_5,  KC_TRANSPARENT,
+  ES_IEXL,        KC_1,       KC_2,      KC_3,     KC_4,      KC_5,  KC_NO,
   KC_CAPSLOCK,    KC_DOT,     KC_COMMA,  ES_NTIL,  KC_P,      KC_Y,  KC_ESCAPE,
   KC_TAB,         KC_A,       KC_O,      KC_E,     KC_U,      KC_I,
-  OSM(MOD_LSFT),  ES_MINS,    KC_Q,      KC_J,     KC_K,      KC_X,  KC_TRANSPARENT,
-  TG(1),          ES_OVRR,    ES_LESS,   KC_LEFT,  KC_RIGHT,
+  OSM(MOD_LSFT),  ES_MINS,    KC_Q,      KC_J,     KC_K,      KC_X,  OSL(_FL),
+  TO(_QL),        ES_OVRR,    ES_LESS,   KC_LEFT,  KC_RIGHT,
 
              OSM(MOD_LCTL), OSM(MOD_LALT),
                             KC_HOME,
@@ -126,15 +99,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // .........................
   // Right hand
 
-  KC_TRANSPARENT,  KC_6,  KC_7,    KC_8,     KC_9,    KC_0,     ES_APOS,
+  KC_NO,           KC_6,  KC_7,    KC_8,     KC_9,    KC_0,     ES_APOS,
   KC_ESCAPE,       KC_F,  KC_G,    KC_C,     KC_H,    KC_L,     ES_CCED,
                    KC_D,  KC_R,    KC_T,     KC_N,    KC_S,     ES_ACUT,
-  KC_TRANSPARENT,  KC_B,  KC_M,    KC_W,     KC_V,    KC_Z,     OSM(MOD_RSFT),
-                          KC_UP,   KC_DOWN,  ES_GRV,  ES_PLUS,  TG(2),
+  OSL(_FL),        KC_B,  KC_M,    KC_W,     KC_V,    KC_Z,     OSM(MOD_RSFT),
+                          KC_UP,   KC_DOWN,  ES_GRV,  ES_PLUS,  TO(_QL),
 
   RALT_T(KC_NO), RCTL_T(KC_NO),
   KC_PGUP,
   KC_PGDOWN,     KC_ENTER,      KC_SPACE),
+
+// ---------------------------------------------
+// Layout 2: one-shot function keys
+
+// This layout replaces the numbers with function keys. This layout is selected
+// from the other layouts by `OSL()` (one-shoot layer), so a key press in this
+// layout returns to the previous one.
+
+  [_FL] = LAYOUT_ergodox(
+
+  // .........................
+  // Left hand
+
+  KC_TRNS,         KC_F1,     KC_F2,     KC_F3,     KC_F4,      KC_F5,     KC_FN11,
+  KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,
+  KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,
+  KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,    KC_TRNS,   KC_TRNS,
+  KC_TRNS,         KC_TRNS,   KC_TRNS,   KC_TRNS,   KC_TRNS,
+
+             KC_TRNS,       KC_TRNS,
+                            KC_TRNS,
+  KC_TRNS,   KC_TRNS,       KC_TRNS,
+
+  // .........................
+  // Right hand
+
+  KC_FN12,          KC_F6,     KC_F7,    KC_F8,      KC_F9,     KC_F10,    KC_TRNS,
+  KC_TRNS,          KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+                    KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,          KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+                               KC_TRNS,  KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+
+  KC_TRNS,       KC_TRNS,
+  KC_TRNS,
+  KC_TRNS,       KC_TRNS,       KC_TRNS),
+
 };
 
 // =============================================================
@@ -160,31 +169,13 @@ uint32_t layer_state_set_user(uint32_t state) {
   ergodox_right_led_2_off();
   ergodox_right_led_3_off();
   switch (layer) {
+    case 0:
+      break;
     case 1:
       ergodox_right_led_1_on();
       break;
     case 2:
       ergodox_right_led_2_on();
-      break;
-    case 3:
-      ergodox_right_led_3_on();
-      break;
-    case 4:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      break;
-    case 5:
-      ergodox_right_led_1_on();
-      ergodox_right_led_3_on();
-      break;
-    case 6:
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
-      break;
-    case 7:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
-      ergodox_right_led_3_on();
       break;
     default:
       break;
